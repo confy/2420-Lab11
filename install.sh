b5b1d9c126a4f28048aaac18a7a4e9d1d502e53a
confy <vagrant@ubuntu2010.localdomain>
2021-03-25 21:44:42 -0700

    corrected permissions

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 if [ ! -d "/home/vagrant/wttr"  ]
 then
-    mkdir "/home/vagrant/wttr"
+    sudo -u vagrant mkdir "/home/vagrant/wttr"
 fi
 curr_dir=$(pwd)
-echo "$curr_dir"
 cp "$curr_dir/wttr" /usr/local/bin
 cp "$curr_dir/wttr.service" /etc/systemd/system
 cp "$curr_dir/wttr.timer" /etc/systemd/system
